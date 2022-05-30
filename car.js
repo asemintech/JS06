4959c704badb4ae565a111adc92afb072950fe1c
Asem <asemandakayeva@gmail.com>
2022-05-30 13:42:46 +0600

    Fixed code style: JS06: HW

--- car.js
+++ car.js
@@ -21,29 +21,26 @@ class Car {
                 maxFuelVolume,
                 fuelConsumption) {
         this.#brand = brand;
-
         this.#model = model;
-
         this.#yearOfManufacturing = yearOfManufacturing;
-
         this.#maxSpeed = maxSpeed;
-
         this.#maxFuelVolume = maxFuelVolume;
-
         this.#fuelConsumption = fuelConsumption;
     }
 
     get brand () {
         return this.#brand;
     }
-    
+
     set brand (carBrand) {
         if (typeof carBrand !== 'string') {
             throw new Error (`Incorrect brand name.`);
         }
+
         if (carBrand.length < 1) {
             throw new Error (`Brand name is too short.`);
         }
+
         if (carBrand.length > 50) {
             throw new Error (`Brand name is too long.`);
         }
@@ -54,14 +51,16 @@ class Car {
     get model () {
         return this.#model;
     }
-    
+
     set model (carModel) {
         if (typeof carModel !== 'string') {
             throw new Error (`Incorrect model name.`);
         }
+
         if (carModel.length < 1) {
             throw new Error (`Model name is too short.`);
         }
+
         if (carModel.length > 50) {
             throw new Error (`Model name is too long.`);
         }
@@ -72,16 +71,18 @@ class Car {
     get yearOfManufacturing () {
         return this.#yearOfManufacturing;
     }
-    
+
     set yearOfManufacturing (carYear) {
         const currentYear = (new Date()).getFullYear();
 
         if (!Number.isInteger(carYear) || carYear <= 0) {
             throw new Error (`Incorrect year.`);
         }
+
         if (carYear < 1900) {
             throw new Error (`Year of manufacturing cannot be earlier than 1900.`);
         }
+
         if (carYear > currentYear) {
             throw new Error (`Year of manufacturing cannot be later than the current year.`);
         }
@@ -97,9 +98,11 @@ class Car {
         if (!Number.isInteger(carMaxSpeed) || carMaxSpeed < 0) {
             throw new Error (`Incorrect max speed.`);
         }
+
         if (carMaxSpeed < 100) {
             throw new Error (`Max speed cannot be less than 100 km/h.`);
         }
+
         if (carMaxSpeed > 300) {
             throw new Error (`Max speed cannot be more than 300 km/h.`);
         }
@@ -110,14 +113,16 @@ class Car {
     get maxFuelVolume() {
         return this.#maxFuelVolume;
     }
-    
+
     set maxFuelVolume(carMaxFuel) {
         if (!Number.isInteger(carMaxFuel) || carMaxFuel < 0) {
             throw new Error (`Incorrect max fuel volume.`);
         }
+
         if (carMaxFuel < 5) {
             throw new Error (`Max fuel volume cannot be less than 5 l.`);
         }
+
         if (carMaxFuel > 20) {
             throw new Error (`Max fuel volume cannot be more than 20 l.`);
         }
@@ -128,7 +133,7 @@ class Car {
     get fuelConsumption() {
         return this.#fuelConsumption;
     }
-    
+
     set fuelConsumption(carFuelConsumption) {
         if (!Number.isInteger(carFuelConsumption) || carFuelConsumption <= 0) {
             throw new Error (`Incorrect fuel consumption.`);
